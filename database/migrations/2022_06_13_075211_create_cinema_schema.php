80bc3b28250c8cea239d0d1f787f895306d7a554
hidayat3676 <hidayat3676@gmail.com>
2023-04-29 14:13:59 +0500

    migration added

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -36,8 +36,63 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
+        Schema::create('show_rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('total_seats');
+            $table->enum('status', ['available', 'running']);
+        });
 
-    }
+        Schema::create('movies', function (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->dateTime('available_for_watching_at');
+            $table->enum('status', ['available', 'booked']);
+            $table->boolean('running_status');
+            $table->timestamps();
+        });
+        Schema::create('movie_show_rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('movie_id')->unsigned();
+            $table->bigInteger('admin_id')->unsigned();
+            $table->bigInteger('show_room_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->cascadeOnDelete();
+            $table->foreign('show_room_id')->references('id')->on('show_rooms')->cascadeOnDelete();
+            $table->foreign('admin_id')->references('id')->on('admins')->cascadeOnDelete();
+            $table->timestamps();
+
+        });
+
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('seats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('price');
+            $table->enum('status', ['available', 'booked']);
+            $table->enum('type', ['couple', 'vip', 'super']);
+            $table->bigInteger('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->cascadeOnDelete();
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('start_date_time');
+            $table->dateTime('end_date_time');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('movie_id')->unsigned();
+            $table->bigInteger('seat_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('seat_id')->references('id')->on('seats')->cascadeOnDelete();
+            $table->foreign('movie_id')->references('id')->on('movies')->cascadeOnDelete();
+            $table->string('status');
+            $table->timestamps();
+        });
+        }
 
     /**
      * Reverse the migrations.
@@ -46,5 +101,11 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::drop('show_rooms');
+        Schema::drop('movies');
+        Schema::drop('movie_show_rooms');
+        Schema::drop('admins');
+        Schema::drop('seats');
+        Schema::drop('bookings');
     }
 }
